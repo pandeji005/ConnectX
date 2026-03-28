8029aa492ab1a589f480a763b3bb6b831cd85e01
pandeji005 <amoghadeshpande@gmail.com>
2026-03-29 01:09:25 +0530

    fix: switching to eventlet-only for stable Socket.io on Render

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, send_file
 from sqlalchemy.exc import IntegrityError
 from flask_sqlalchemy import SQLAlchemy
